0f4aa41fbbf322a243a3d1ab4ea06ea33895bf78
Michael Harfenist <hrharfenist@gmail.com>
2021-10-06 16:06:35 -0400

    for deployement, products commented out

--- server/models/products.js
+++ server/models/products.js
@@ -1,16 +1,16 @@
-const db = require('../database/products/index.js');
+// const db = require('../database/products/index.js');
 
-module.exports = {
-  getAll: function (callback) {
-    var queryString = "SELECT * FROM products LIMIT 5";
-    db.query(queryString, (err, res) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(res);
-      }
-    });
-  },
-};
+// module.exports = {
+//   getAll: function (callback) {
+//     var queryString = "SELECT * FROM products LIMIT 5";
+//     db.query(queryString, (err, res) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(res);
+//       }
+//     });
+//   },
+// };
 
 // getAll();
